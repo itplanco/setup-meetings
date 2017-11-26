8752d93485f875b110a2521893dc36179e193b5f
Yuto Nagano <chamelion@hotmail.co.jp>
2017-11-26 23:23:27 +0900

    fix deploy.cmd.

--- deploy.cmd
+++ deploy.cmd
@@ -86,7 +86,7 @@ call :ExecuteCmd npm run build
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 5. Copy dist folder
-IF NOT EXISTS "%DEPLOYMENT_TEMP%\wwwroot" (
+IF NOT EXIST "%DEPLOYMENT_TEMP%\wwwroot" (
   mkdir "%DEPLOYMENT_TEMP%\wwwroot"
 )
 call :ExecuteCmd xcopy dist "%DEPLOYMENT_TEMP%\wwwroot" /Y
